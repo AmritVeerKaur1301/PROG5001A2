8398f77aa349970fb7f5c0f3db3e288c28c45234
AmritVeerKaur1301 <amrit.veer.kaur.10@student.scu.edu.au>
2021-05-03 05:24:18 +0530

    GameBoard class update
    new methods are added to the class.

--- AVK_GameBoard.java
+++ AVK_GameBoard.java
@@ -7,16 +7,35 @@ public class AVK_GameBoard extends JPanel {
     int xcells = 30;    
     int ycells = 30;
     int szcell = 10;        
-       
+
     public AVK_GameBoard() {
         setPreferredSize(new Dimension(xcells * szcell, ycells * szcell));        
-        setBackground(Color.black);
+        setBackground(Color.black); // to set the background color of gameboard
         setFocusable(true);        
+    }
+
+    /**
+     * This collision method to check the collsion of snake with the board and itself.
+     */
+    public void collision()
+    {
+        // put your code here
+
+    }
+
+    /**
+     * game over to show the message that game is over after the collision
+     *
+     */
+    public void gameover()
+    {
+        // put your code here
+        
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
     }
-    
+
 }

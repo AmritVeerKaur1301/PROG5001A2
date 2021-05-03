35966bba1285a8da470d7db4966de70755023c4d
AmritVeerKaur1301 <amrit.veer.kaur.10@student.scu.edu.au>
2021-05-03 05:30:51 +0530

    Update Snake
    New methods are added for snake game

--- Snake.java
+++ Snake.java
@@ -4,13 +4,17 @@
  * @AMRIT VEER KAUR 
  * @Snakegame 1.0(date - 25-04-2021)
  */
-public class Snake
+import javax.swing.JFrame;
+public class Snake extends JFrame       // to add the frame and its properties to the snake game.
 {
     /**
      * Constructor for objects of class Snake
      */
     public Snake() {
-        // initialise instance variables        
+        // initialise instance variables   
+        setTitle("Snake");      // to set the game title
+        
     }
 
+    
 }

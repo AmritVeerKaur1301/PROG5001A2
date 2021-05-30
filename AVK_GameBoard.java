b53377ad531504d81d8413868330f5e1b0cae570
AmritVeerKaur1301 <amrit.veer.kaur.10@student.scu.edu.au>
2021-05-30 11:44:59 +0530

    GAmeboard
    some changes regarding playing of game are changed.

--- AVK_GameBoard.java
+++ AVK_GameBoard.java
@@ -12,34 +12,41 @@ import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /*
  * Gameboard class is created to add the snake, prey to the board to play game.
  * to move the snake and after eaten the prey to display on random location.
- * @author Amrit Veer Kaur
  */
 
 public class AVK_GameBoard extends JPanel implements ActionListener {
-
-    private final int GB_Width = 320;
-    private final int GB_Hgt = 320;
+    
+    private final int GB_Width = 300;
+    private final int GB_Hgt = 400;
     private final int S_dot = 10;
-    private final int ALL_DOTS = 920;
+    private final int ALL_DOTS = 950;
     private final int RAND_POS = 28;
-    private final int DELAY = 120;
+    private final int DELAY = 115;
 
     private final int x[] = new int[ALL_DOTS];
     private final int y[] = new int[ALL_DOTS];
 
-    private int size;
+     int snake_length;
     private int prey_x;
     private int prey_y;
     int score = 0;
+    int s;
     
-    private boolean leftDirection = false;
-    private boolean rightDirection = true;
-    private boolean upDirection = false;
-    private boolean downDirection = false;
+    
+    private boolean left_side = false;
+    private boolean right_side = false;   
+    private boolean up_side = false;
+    private boolean down_side = true;// snake will move towards the down direction at starting
+    
     private boolean playGame = true;
     private boolean pauseGame = false;
 
@@ -47,17 +54,37 @@ public class AVK_GameBoard extends JPanel implements ActionListener {
     private Image snakeb;
     private Image smily;
     private Image snakeh;
+    
+    private JLabel labelgameover;
+    private JLabel labelplay;  
 
     public AVK_GameBoard() { 
         // setting the panel background, size and apply key functions to play game.
-        addKeyListener(new TAdapter());
+        addKeyListener(new click_action()); 
         setBackground(Color.black);
         setFocusable(true);
-
+        setBorder(BorderFactory.createLineBorder(new Color(0,0,0), 1));
         setPreferredSize(new Dimension(GB_Width, GB_Hgt));
         uploadImages();    // to upload the images of snake and prey
         initGame();
         
+        labelgameover = new JLabel("Game Over",JLabel.CENTER);
+        labelplay = new JLabel("Click to Play"); 
+        labelplay.addMouseListener(new MouseAdapter()  
+       { 
+            public void mouseClicked(MouseEvent e)  
+            {  
+       
+              //AVK_GameBoard();
+              
+              AVK_SnakeGame s = new AVK_SnakeGame();
+              s.setVisible(true);
+              
+              
+              
+            }  
+    });
+
     }
     
     private void uploadImages() {
@@ -73,9 +100,9 @@ public class AVK_GameBoard extends JPanel implements ActionListener {
 
     private void initGame() {
 
-        size = 4;
+        snake_length = 4;
 
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < snake_length; i++) {
             x[i] = 50 - i * 10;
             y[i] = 50;
         }
@@ -99,72 +126,86 @@ public class AVK_GameBoard extends JPanel implements ActionListener {
 
             g.drawImage(smily, prey_x, prey_y, this);
 
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < snake_length; i++) {
                 if (i == 0) {
                     g.drawImage(snakeh, x[i], y[i], this);
-                } else {
-                    g.drawImage(snakeb, x[i], y[i], this);
+                } else { g.drawImage(snakeb, x[i], y[i], this);
                 }
             }
 
             Toolkit.getDefaultToolkit().sync();
 
         } else {
-            
             gameOver(g);
-           
-        }        
     }
+}
 
     private void gameOver(Graphics g) {
-    
-        AVK_Scoreframe sf = new AVK_Scoreframe();
-        sf.setVisible(true);
+        
+       
+        setLayout(new GridLayout(2,1));
+        labelplay.setForeground(new Color(255,255,255));
+        labelgameover.setForeground(new Color(255,255,255));
+        labelplay.setHorizontalAlignment(JLabel.CENTER);
+        
+        labelgameover.setHorizontalAlignment(JLabel.CENTER);
+        add(labelplay);
+        add(labelgameover);
+        
+        
          
     }
 
-    private void checkPrey() {
+    public void checkPrey() {
         
         if ((x[0] == prey_x) && (y[0] == prey_y)) {
             
-            size++;
-            
+            snake_length++;
             score = score + 1;
-            System.out.println(score);
+            
+            System.out.println("Score = " + score);
+            System.out.println("length = " + snake_length);
+            
+            
             locatePrey();
         }
         
     }
 
+     public int getscore() {
+        return s;
+    }
+
+    
     private void movesnake() {
 
-        for (int i = size; i > 0; i--) {
+        for (int i = snake_length; i > 0; i--) {
             x[i] = x[(i - 1)];
             y[i] = y[(i - 1)];
         }
 
-        if (leftDirection) {
+        if (left_side) {
             x[0] -= S_dot;
         }
 
-        if (rightDirection) {
+        if (right_side) {
             x[0] += S_dot;
         }
 
-        if (upDirection) {
+        if (up_side) {
             y[0] -= S_dot;
         }
 
-        if (downDirection) {
+        if (down_side) {
             y[0] += S_dot;
         }
     }
 
     private void checkhit() {
 
-        for (int i = size; i > 0; i--) {
+        for (int i = snake_length; i > 0; i--) {
 
-            if ((i > 4) && (x[0] == x[i]) && (y[0] == y[i])) {
+            if ((i > 4) && (x[0] == x[i]) && (y[0] == y[i])) { // i>4
                 playGame = false;
             }
         }
@@ -212,7 +253,7 @@ public class AVK_GameBoard extends JPanel implements ActionListener {
         repaint();
     }
 
-    private class TAdapter extends KeyAdapter {
+    private class click_action extends KeyAdapter {
 
         @Override
         public void keyPressed(KeyEvent e) {
@@ -233,29 +274,31 @@ public class AVK_GameBoard extends JPanel implements ActionListener {
            
             // code to change the direction of snake with the keys left, right, up and down
             
-            if ((key == KeyEvent.VK_LEFT) && (!rightDirection)) {
-                leftDirection = true;
-                upDirection = false;
-                downDirection = false;
+            if ((key == KeyEvent.VK_LEFT) && (!right_side)) {
+                left_side = true;
+                up_side = false;
+                down_side = false;
             }
 
-            if ((key == KeyEvent.VK_RIGHT) && (!leftDirection)) {
-                rightDirection = true;
-                upDirection = false;
-                downDirection = false;
+            if ((key == KeyEvent.VK_RIGHT) && (!left_side)) {
+                right_side = true;
+                up_side = false;
+                down_side = false;
             }
 
-            if ((key == KeyEvent.VK_UP) && (!downDirection)) {
-                upDirection = true;
-                rightDirection = false;
-                leftDirection = false;
+            if ((key == KeyEvent.VK_UP) && (!down_side)) {
+                up_side = true;
+                right_side = false;
+                left_side = false;
             }
 
-            if ((key == KeyEvent.VK_DOWN) && (!upDirection)) {
-                downDirection = true;
-                rightDirection = false;
-                leftDirection = false;
+            if ((key == KeyEvent.VK_DOWN) && (!up_side)) {
+                down_side = true;
+                right_side = false;
+                left_side = false;
             }
         }
+        
     }
+
 }

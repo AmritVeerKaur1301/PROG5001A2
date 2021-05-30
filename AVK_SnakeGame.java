7a07346eeba0fde97c26060ac61647f113d5b74a
AmritVeerKaur1301 <amrit.veer.kaur.10@student.scu.edu.au>
2021-05-30 11:46:59 +0530

    snake game
    two different panels are added to frame of snake game class

--- AVK_SnakeGame.java
+++ AVK_SnakeGame.java
@@ -1,6 +1,8 @@
 
 import java.awt.EventQueue;
 import javax.swing.JFrame;
+import java.awt.GridLayout;
+
 /*
  * Class to make frame for the snake game.
  * @author Amrit Veer Kaur
@@ -9,14 +11,19 @@ public class AVK_SnakeGame extends JFrame {
 
     public AVK_SnakeGame() {
         
-        add(new AVK_GameBoard());  // to add gameboard panel into frame
-        
+        setLayout(new GridLayout(1, 2,2,2));
         setResizable(false);
         pack();
+        setBounds(10,10,600,400);
         
-        setTitle("Snake Game");  // to set title of the game
+        setTitle("Snake Game: Amrit");  // to set title of the game
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        AVK_GameBoard gb = new AVK_GameBoard();
+        add(gb);  // to add gameboard panel into frame
+        
+        AVK_Scoreframe sf = new AVK_Scoreframe();
+        add(sf);
         
         
     }
